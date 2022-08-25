7704e42d9c626170a98965c8245f2fd2d92488a3
Fran M <fr.me.jim@gmail.com>
2022-08-25 16:15:24 +0200

    adding csrf token logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useCallback } from 'react';
 import { BrowserRouter as Router} from "react-router-dom";
 
 
@@ -9,7 +9,22 @@ import './css/main.css';
 import Navbar from './components/layout/Navbar';
 import MainWrapper from './components/layout/MainWrapper';
 
+// service
+import { getCSRFTokenService } from "./service/auth.services";
+
 const App: FC = () => {
+
+  const getCRSFToken = useCallback(
+    getCSRFTokenService,
+    [],
+  );
+
+  useEffect(() => {
+    const queryToAPI = async () => await getCRSFToken();
+  
+    queryToAPI();
+  }, [ getCRSFToken ]);
+  
   return (
     <Router>
       <Navbar />
--- src/service/auth.services.ts
+++ src/service/auth.services.ts
@@ -4,7 +4,30 @@ import axiosInstance from './index';
 import { IAuthUserData, ISigninUserData } from '../interfaces/auth.interface';
 import { AxiosError } from 'axios';
 
+export const getCSRFTokenService = async (): Promise<void> => {
+    try {
+        const response = await axiosInstance.get('/auth/csrf');
+
+        if (response.status === 401) {
+            throw new AxiosError("Wrong Credentials!", `${response.status}`);
+        }
+
+        const csrf: string = response.data.csrf;
+
+        if (csrf) axiosInstance.defaults.headers.post['XSRF-TOKEN'] = csrf;
+    } catch (error: unknown) {
+        if (error instanceof AxiosError) {
+            if (error.code === '401') {
+                throw new Error("Wrong Credentials!");
+            }
+        }
+
+        throw new Error("Something went wrong, try again!");
+    }
+};
+
 export const postLoginService = async (email: string, password: string): Promise<IAuthUserData> => {
+    console.log(axiosInstance.defaults.headers.post)
     try {
         const response = await axiosInstance.post('/auth/login', {
             email,
